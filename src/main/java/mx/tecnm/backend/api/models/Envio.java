7176e017d0a55ad3bee4224cf0841b67e33039a8
Ironman9991 <149987137+Ironman9991@users.noreply.github.com>
2025-12-04 11:47:00 -0600

    Endpoints funcionales

--- src/main/java/mx/tecnm/backend/api/models/Envio.java
+++ src/main/java/mx/tecnm/backend/api/models/Envio.java
@@ -1,6 +1,6 @@
 package mx.tecnm.backend.api.models;
 
 public class Envio {
- public record envio(int id, String fecha, String numero_seguimiento, String estado) {
+ public record envio(int id, String fecha_envio, String numero_seguimiento, String estado_envio) {
  }
 }
